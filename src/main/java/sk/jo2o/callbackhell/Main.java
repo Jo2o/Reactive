9902e134336b29ec024eb07aefaf50748522763d
JozefMikus <jozef.mikus@kyndryl.com>
2022-06-14 08:27:46 +0200

    -- callback hell

--- src/main/java/sk/jo2o/callbackhell/Main.java
+++ src/main/java/sk/jo2o/callbackhell/Main.java
@@ -1,12 +1,30 @@
 package sk.jo2o.callbackhell;
 
+import java.util.concurrent.TimeUnit;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        log.info("Hello");
+        log.info("Main thread running ...");
+        
+        Runnable runnable = () -> new Main().runningAsync(new MyCallback());
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        TimeUnit.SECONDS.sleep(10);
+        
+        log.info("Main thread completed!");
+    }
+    
+    @SneakyThrows
+    public void runningAsync(MyCallback myCallback) {
+        log.info("Running in another thread ...");
+        TimeUnit.SECONDS.sleep(2);
+        myCallback.call();
     }
 
 }
--- src/main/java/sk/jo2o/callbackhell/MyCallback.java
+++ src/main/java/sk/jo2o/callbackhell/MyCallback.java
@@ -1,5 +1,11 @@
 package sk.jo2o.callbackhell;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MyCallback {
 
+    public void call() {
+        log.info("Callback called ...");
+    }
 }
